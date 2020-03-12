9715ba4766782abb237fe852b3187f4adfada09a
cpwan <cpwan@ust.hk>
2020-03-12 13:38:46 +0000

    added command line entry

---
+++ main.py
@@ -0,0 +1,10 @@
+import parser
+import _main
+if __name__=="__main__":
+    args=parser.parse_args()
+    print("#"*64)
+    for i in vars(args):
+        print(f"#{i:>40}: {str(getattr(args,i)):<20}#")
+    print("#"*64)     
+    _main.main(args)
+                        
